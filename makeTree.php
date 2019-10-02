60469d200e06c97c2b68fe73f0d18138d072d236
AlexP007 <41651899+AlexP007@users.noreply.github.com>
2019-10-02 22:26:53 +0300

    Update makeTree.php

--- makeTree.php
+++ makeTree.php
@@ -4,12 +4,10 @@
 function createTreeMenu(&$array) {
     array_walk($array, function(&$val, $key) use (&$array) {
         if ($val['IS_PARENT']) {
-            debug($val);
             $childs = array_filter($array, function ($elt) use ($val) {
                 return $elt['CHAIN'][0] === $val['TEXT'] && $elt['TEXT'] !== $val['TEXT'];
             });
             $val['CHILDS'] = $childs;
-            debug($val);
             for ($i = 1; $i <= count($childs); $i++) {
                 unset($array[$key + $i]);
             }
